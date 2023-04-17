dd47650eba331e8723dc3340b22c5a0ccf95fad7
FatimaMary <111224454+FatimaMary@users.noreply.github.com>
2023-04-17 11:32:55 +0530

    NavBar Finished

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,10 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import {createTheme} from "@mui/material/styles";
 import { useMemo } from "react";
 import { useSelector } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { themeSettings } from "theme";
+import Layout from "scenes/layout";
+import Dashboard from "scenes/dashboard";
 
 function App() {
   const mode = useSelector((state) => state.global.mode);
@@ -13,6 +15,12 @@ function App() {
       <BrowserRouter>
         <ThemeProvider theme={theme}>
             <CssBaseline/>
+            <Routes>
+              <Route element={<Layout/>}>
+                <Route path="/" element={<Navigate to="/dashboard" replace/>}/>
+                <Route path="/dashboard" element={<Dashboard/>}/>
+              </Route>
+            </Routes>
         </ThemeProvider>
       </BrowserRouter>
     </div>
